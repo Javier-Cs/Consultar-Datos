5ebf823c26c317b57c99d4f58a21cc0473cb895b
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-01-12 14:39:18 -0500

    Agregar archivos de proyecto.

---
+++ ConsultarDatos/Modelos/ResponseApi/DatosLicenciaConducir.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsultarDatos.Modelos.ResponseApi
+{
+    public class DatosLicenciaConducir
+    {
+        // COLUMNAS PARA LICENCIA
+        public string? Email { get; set; }
+        public string? Telefono { get; set; }
+        public string? Celular { get; set; }
+        public string? TipoLicencia { get; set; }
+        public string? LicenciaFechaDesde { get; set; }
+        public string? LicenciaFechaHasta { get; set; }
+        public string? TipoSangre { get; set; }
+        public DateTime? FechaDefuncion { get; set; }
+        public string? LugarDefuncion { get; set; }
+        public string? MotivoDefuncion { get; set; }
+    }
+}
---
+++ ConsultarDatos/Modelos/ResponseApi/DatosPersonaModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsultarDatos.Modelos.ResponseApi
+{
+    public class DatosPersonaModel
+    {
+        public string? Cedula {  get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaNacimiento {  get; set; }
+        public string? EstadoCivil { get; set; }
+        public string? Conyugue { get; set; }
+        public DateTime? FechaCedulacion {  get; set; }
+        public string? LugarDomicilio {  get; set; }
+        public string? CalleDomiciliop {  get; set; }
+        public string? NumeroDomicilio {  get; set; }
+        public string? NombresMadre {  get; set; }
+        public string? NombresPadre {  get; set; }
+        public string? LugarNacimiento {  get; set; }
+        public DateTime? FechaConsulta {  get; set; }
+        public DateTime? FechaExpiracion {  get; set; }
+        public string? Genero {  get; set; }
+        public string? Nacionalidad {  get; set; }
+        public string? Instruccion {  get; set; }
+        public string? ProfecionActual {  get; set; }
+        public string? condicion {  get; set; }
+
+        public List<DatosLicenciaConducir> DatosDeLicenciaDeConducir { get; set; } = new List<DatosLicenciaConducir>();
+
+    }
+}
